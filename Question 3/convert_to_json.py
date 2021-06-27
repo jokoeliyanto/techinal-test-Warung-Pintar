a772e2b22937c04af55343f4446c20553ebb44ab
Joko Eliyanto <57888234+jokoeliyanto@users.noreply.github.com>
2021-06-27 18:43:09 +0700

    Create convert_to_json.py

---
+++ Question 3/convert_to_json.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import json
+
+df = pd.read_csv('https://raw.githubusercontent.com/jokoeliyanto/techinal-test-Warung-Pintar/main/%5BTechnical%20Test%20-%20Data%20Engineer%5D%20Sale%20Report%20-%20wp.csv')
+
+result = df.to_json(orient="columns")
+
+with open('json_file.json', 'w') as json_file:
+    json.dump(result, json_file)
